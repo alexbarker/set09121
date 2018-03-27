45c7f02a8c5a25ce95c17face8884b9a61514b88
DESKTOP-AMDFDIA\alexb <40333139@live.napier.ac.uk>
2018-03-27 23:45:15 +0100

    Version 9.1.0 - Completed AI: Making Decisions.

--- practical_8/components/cmp_ai_steering.h
+++ practical_8/components/cmp_ai_steering.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <ecm.h>
 #include "../steering.h"
 
--- practical_8/main.cpp
+++ practical_8/main.cpp
@@ -1,1 +1,13 @@
-// Hello
+#include "engine.h"
+#include "game.h"
+#include "scenes/scene_menu.h"
+
+using namespace std;
+
+MenuScene menu;
+StateScene stateScene;
+DecisionScene decisionScene;
+
+int main() {
+	Engine::Start(1280, 720, "Decision", &menu);
+}
