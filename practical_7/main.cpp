2cf9346240f4eed4a12096a86b8d40d154feea2d
DESKTOP-AMDFDIA\alexb <40333139@live.napier.ac.uk>
2018-03-27 22:39:34 +0100

    Version 8.1.0 - Completed AI: Steering and Pathfinding. Did not implement arrive steering behaviour.

--- practical_7/main.cpp
+++ practical_7/main.cpp
@@ -1,1 +1,13 @@
-// Hello
+#include "engine.h"
+#include "game.h"
+#include "scenes/scene_menu.h"
+
+using namespace std;
+
+MenuScene menu;
+SteeringScene steeringScene;
+PathfindingScene pathfindingScene;
+
+int main() {
+	Engine::Start(1280, 720, "Steering", &menu);
+}
