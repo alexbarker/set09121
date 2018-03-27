0a49fc125d80d6ca2deef8e17149a5c6708db3c3
DESKTOP-AMDFDIA\alexb <40333139@live.napier.ac.uk>
2018-03-27 21:24:05 +0100

    Version 7.1.0 - Completed Platformer. All three levels are playable in a loop until exiting.

--- practical_6/scenes/scene_level1.cpp
+++ practical_6/scenes/scene_level1.cpp
@@ -5,41 +5,43 @@
 #include <LevelSystem.h>
 #include <iostream>
 #include <thread>
-
 using namespace std;
 using namespace sf;
 
 static shared_ptr<Entity> player;
 
 void Level1Scene::Load() {
-  cout << " Scene 1 Load" << endl;
+  cout << "Scene 1 Load" << endl;
   ls::loadLevelFile("res/level_1.txt", 40.0f);
-
   auto ho = Engine::getWindowSize().y - (ls::getHeight() * 40.f);
   ls::setOffset(Vector2f(0, ho));
 
   // Create player
   {
-    player = makeEntity();
-    player->setPosition(ls::getTilePosition(ls::findTiles(ls::START)[0]));
-    auto s = player->addComponent<ShapeComponent>();
-    s->setShape<sf::RectangleShape>(Vector2f(20.f, 30.f));
-    s->getShape().setFillColor(Color::Magenta);
-    s->getShape().setOrigin(10.f, 15.f);
-
-    player->addComponent<PlayerPhysicsComponent>(Vector2f(20.f, 30.f));
+	  // *********************************
+	  player = makeEntity();
+	  player->setPosition(ls::getTilePosition(ls::findTiles(ls::START)[0]));
+	  auto s = player->addComponent<ShapeComponent>();
+	  s->setShape<sf::RectangleShape>(Vector2f(20.f, 30.f));
+	  s->getShape().setFillColor(Color::Yellow);
+	  s->getShape().setOrigin(10.f, 15.f);
+	  // *********************************
+	  player->addTag("player");
+	  player->addComponent<PlayerPhysicsComponent>(Vector2f(20.f, 30.f));
   }
 
   // Add physics colliders to level tiles.
   {
-    auto walls = ls::findTiles(ls::WALL);
-    for (auto w : walls) {
-      auto pos = ls::getTilePosition(w);
-      pos += Vector2f(20.f, 20.f); //offset to center
-      auto e = makeEntity();
-      e->setPosition(pos);
-      e->addComponent<PhysicsComponent>(false, Vector2f(40.f, 40.f));
-    }
+	  // *********************************
+	  auto walls = ls::findTiles(ls::WALL);
+	  for (auto w : walls) {
+		  auto pos = ls::getTilePosition(w);
+		  pos += Vector2f(20.f, 20.f);
+		  auto e = makeEntity();
+		  e->setPosition(pos);
+		  e->addComponent<PhysicsComponent>(false, Vector2f(40.f, 40.f));
+	  }
+	  // *********************************
   }
 
   //Simulate long loading times
@@ -50,7 +52,7 @@ void Level1Scene::Load() {
 }
 
 void Level1Scene::UnLoad() {
-  cout << "Scene 1 Unload" << endl;
+  cout << "Scene 1 UnLoad" << endl;
   player.reset();
   ls::unload();
   Scene::UnLoad();
@@ -58,10 +60,21 @@ void Level1Scene::UnLoad() {
 
 void Level1Scene::Update(const double& dt) {
 
+
+/*
   if (ls::getTileAt(player->getPosition()) == ls::END) {
     Engine::ChangeScene((Scene*)&level2);
   }
   Scene::Update(dt);
+  */
+	Scene::Update(dt);
+	const auto pp = player->getPosition();
+	if (ls::getTileAt(pp) == ls::END) {
+		Engine::ChangeScene((Scene*)&level2);
+	}
+	else if (!player->isAlive()) {
+		Engine::ChangeScene((Scene*)&level1);
+	}
 }
 
 void Level1Scene::Render() {
--- practical_6/scenes/scene_level2.cpp
+++ practical_6/scenes/scene_level2.cpp
@@ -11,6 +11,7 @@ using namespace std;
 using namespace sf;
 
 static shared_ptr<Entity> player;
+
 void Level2Scene::Load() {
   cout << "Scene 2 Load" << endl;
   ls::loadLevelFile("res/level_2.txt", 40.0f);
@@ -19,14 +20,14 @@ void Level2Scene::Load() {
 
   // Create player
   {
-    // *********************************
-
-
-
-
-
-
-    // *********************************
+	  // *********************************
+	  player = makeEntity();
+	  player->setPosition(ls::getTilePosition(ls::findTiles(ls::START)[0]));
+	  auto s = player->addComponent<ShapeComponent>();
+	  s->setShape<sf::RectangleShape>(Vector2f(20.f, 30.f));
+	  s->getShape().setFillColor(Color::Blue);
+	  s->getShape().setOrigin(10.f, 15.f);
+	  // *********************************
     player->addTag("player");
     player->addComponent<PlayerPhysicsComponent>(Vector2f(20.f, 30.f));
   }
@@ -36,17 +37,17 @@ void Level2Scene::Load() {
     auto enemy = makeEntity();
     enemy->setPosition(ls::getTilePosition(ls::findTiles(ls::ENEMY)[0]) +
                        Vector2f(0, 24));
-    // *********************************
-    // Add HurtComponent
-
-    // Add ShapeComponent, Red 16.f Circle
-
-
-
-
-    // Add EnemyAIComponent
-
-    // *********************************
+	// *********************************
+	// Add HurtComponent
+	enemy->addComponent<HurtComponent>();
+	// Add ShapeComponent, Red 16.f Circle
+	auto s = enemy->addComponent<ShapeComponent>();
+	s->setShape<CircleShape>(16.f, 16.f);
+	s->getShape().setFillColor(Color::Red);
+	s->getShape().setOrigin(Vector2f(16.f, 16.f));
+	// Add EnemyAIComponent
+	enemy->addComponent<EnemyAIComponent>();
+	// *********************************
   }
 
   // Create Turret
@@ -63,16 +64,16 @@ void Level2Scene::Load() {
 
   // Add physics colliders to level tiles.
   {
-    // *********************************
-
-
-
-
-
-
-
-
-    // *********************************
+	  // *********************************
+	  auto walls = ls::findTiles(ls::WALL);
+	  for (auto w : walls) {
+		  auto pos = ls::getTilePosition(w);
+		  pos += Vector2f(20.f, 20.f);
+		  auto e = makeEntity();
+		  e->setPosition(pos);
+		  e->addComponent<PhysicsComponent>(false, Vector2f(40.f, 40.f));
+	  }
+	  // *********************************
   }
 
   cout << " Scene 2 Load Done" << endl;
